79fdaa109903cdeea01ebc237fe9d695c599d0b1
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-20 13:53:59 -0700

    add custom usernames on chat and location messages

--- src/index.js
+++ src/index.js
@@ -27,32 +27,34 @@ io.on('connection', (socket) => {
 
     socket.join(user.room)
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`)) // emits to every connected client but the current one
+    socket.emit('message', generateMessage('Admin', 'Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`)) // emits to every connected client but the current one
 
     callback()
   })
 
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
     const filter = new Filter()
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!')
     }
 
-    io.to().emit('message', generateMessage(message))
+    io.to(user.room).emit('message', generateMessage(user.username, message))
     callback()
   })
 
   socket.on('sendLocation', (location, callback) => {
-    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?=${location.latitude},${location.longitude}`))
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username,`https://google.com/maps?=${location.latitude},${location.longitude}`))
     callback()
   })
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
     if(user) {
-      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
     }
   })
 })
