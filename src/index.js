633b729bc5a3c5267913fbf4c521b2feeb7647c4
Dan Seagren <seagrendaniel562@gmail.com>
2020-08-20 13:31:47 -0700

    add initial suer tracking

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
-const {generateMessage, generateLocationMessage} = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -17,15 +18,22 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
   console.log('New Web Socket connection')
 
-  socket.on('join', ({username, room}) => {
-    socket.join(room)
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if(error) {
+      return callback(error)
+    }
+
+    socket.join(user.room)
 
     socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`)) // emits to every connected client but the current one
-  
+    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`)) // emits to every connected client but the current one
+
+    callback()
   })
 
-  socket.on('sendMessage', (message, callback) => { 
+  socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
 
     if (filter.isProfane(message)) {
@@ -42,7 +50,10 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', generateMessage('A user has left!'))
+    const user = removeUser(socket.id)
+    if(user) {
+      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+    }
   })
 })
 
